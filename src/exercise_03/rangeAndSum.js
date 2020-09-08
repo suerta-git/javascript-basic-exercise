c63e6acbc876931f742c891649f43608475e2936
Suerta <suerta__@hotmail.com>
2020-09-08 11:25:34 +0800

    [Zhang Jindong] feat: pass exercise 3

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,17 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  const resultArray = [];
+  if (start <= end) {
+    for (let num = start; num < end; num += 1) {
+      resultArray.push(num);
+    }
+  } else {
+    for (let num = start; num > end; num -= 1) {
+      resultArray.push(num);
+    }
+  }
+  return resultArray;
 }
 
 export function sum(...numbers) {
@@ -17,6 +27,5 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  return numbers.reduce((previous, current) => previous + current, 0);
 }
