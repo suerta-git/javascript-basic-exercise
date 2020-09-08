36f94f8a331a1fc0098a21e00b86534861657707
Suerta <suerta__@hotmail.com>
2020-09-08 11:49:21 +0800

    [Zhang Jindong] feat: pass exercise 4

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,24 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  function Node(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  const startNode = new Node(array[0]);
+  let previousNode = startNode;
+  for (let index = 1; index < array.length; index += 1) {
+    const newNode = new Node(array[index]);
+    previousNode.next = newNode;
+    previousNode = newNode;
+  }
+  return startNode;
 }
