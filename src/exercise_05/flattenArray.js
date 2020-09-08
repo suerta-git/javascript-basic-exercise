72ceea62b7a66b7f1e7ded3da5374d8b019c0871
Suerta <suerta__@hotmail.com>
2020-09-08 12:37:26 +0800

    [Zhang Jindong] feat: pass exercise 5

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,28 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  let level = 0;
+
+  function flatten(nestedArray) {
+    level += 1;
+    if (level >= 2) {
+      return nestedArray;
+    }
+    const result = [];
+    nestedArray.forEach((value) => {
+      if (Array.isArray(value)) {
+        result.push(...flatten(value));
+      } else {
+        result.push(value);
+      }
+    });
+    level -= 1;
+    return result;
+  }
+
+  return flatten(array);
 }
